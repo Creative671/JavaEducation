056b7d01c147f46f07d276b8bc00b788175a8f73
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2022-02-19 14:27:02 +0300

    Базовые конструкции

---
+++ JavaLevel1/BasicConstructions/src/main/Start.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Start {
+    public static void main(String[] args){
+
+        int a = 10;
+        long b = 100_920_203_923L;
+        double s = 23.32d;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
