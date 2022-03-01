511651bbcf679b2ef577e1c24b120ff328edb6a8
IvanVerkhovskiy <IvanVerkhovskiy@yandex.ru>
2022-03-01 14:42:30 +0300

    Update StartApp.java
    Работа с массивами

--- JavaLevel1/Lesson1-2/src/main/StartApp.java
+++ JavaLevel1/Lesson1-2/src/main/StartApp.java
@@ -3,7 +3,7 @@ package main;
 import java.util.Arrays;
 
 public class StartApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /*Массивы*/
         int[] number; // объявление массива
         number = new int[10]; // выделение памяти массива на 10 элементов
@@ -17,7 +17,7 @@ public class StartApp {
 
         /*Массив textNumber*/
         int[] textNumber = {1, 2, 3, 4, 5};
-        int [][] textNumberArrays = {{1, 1}, {2, 2}, {5, 5}};
+        int[][] textNumberArrays = {{1, 1}, {2, 2}, {5, 5}};
         System.out.println("Вывод двойного массива " + Arrays.toString(textNumberArrays));
         System.out.println("Вывод массива " + Arrays.toString(textNumber));
         /*Массив myArray*/
@@ -39,7 +39,7 @@ public class StartApp {
 
         /*Вывод массива на экран через цикл for*/
 
-        for (int i = 0; i < season.length; i++){
+        for (int i = 0; i < season.length; i++) {
             System.out.print(season[i] + ", ");
         }
 
@@ -49,8 +49,8 @@ public class StartApp {
         int[][] arrays = new int[8][8];
 
         /*Поле из двумерного массива*/
-        for (int i = 0; i < arrays.length; i++){
-            for (int j = 0; j < arrays.length; j++){
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = 0; j < arrays.length; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -69,20 +69,54 @@ public class StartApp {
         System.out.println(Arrays.binarySearch(arrayNumber, 78));
 
         System.out.println("Задача 1: ");
-        int a = 5;
-        int b = 5;
-        twoNumberSum(a, b);
+        for (int i = 0; i < 10; i++) {
+            int a = (int) (Math.random() * 10);
+            int b = (int) (Math.random() * 10);
+            System.out.println(a + " + " + b + " = " + twoNumberSum(a, b));
+        }
+
+        System.out.println("Задача 2: ");
+        int[] numberValue = {-1, 2, 65, -2, 3, -10, 3, 0};
+        for (int i = 0; i < numberValue.length; i++) {
+            System.out.println(numberValue[i] + " = " + checkNumber(numberValue[i]));
+        }
+
+        System.out.println("Задача 3: ");
+        printTextValue("Test", 3);
     }
 
 //    Написать метод, принимающий на вход два целых числа и проверяющий,
 //    что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true,
 //    в противном случае – false.
 
-    public static boolean twoNumberSum(int a, int b){
-        if ((a+b) >= 10 && (a+b) <= 20){
+    public static boolean twoNumberSum(int a, int b) {
+        if ((a + b) >= 10 && (a + b) <= 20) {
             return true;
         } else {
             return false;
+        }
+    }
+
+//    Написать метод, которому в качестве параметра передается целое число,
+//    метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+//    Замечание: ноль считаем положительным числом.
+
+    public static boolean checkNumber(int a) {
+        if (a >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+//    Написать метод, которому в качестве аргументов передается строка и число,
+//    метод должен отпечатать в консоль указанную строку, указанное количество раз;
+
+    public static void printTextValue(String text, int number){
+        int a = 0;
+        while (a < number){
+            System.out.println(text);
+            a++;
         }
     }
 }
